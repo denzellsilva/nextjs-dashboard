bc2a08949e8549285a3a39c92dc5750208d84d3d
denzellsilva <silvamarldenzell@gmail.com>
2025-10-08 02:04:56 +0800

    feat: add authentication

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -105,6 +105,7 @@ export default function EditInvoiceForm({
                   value="pending"
                   defaultChecked={invoice.status === "pending"}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="status-error"
                 />
                 <label
                   htmlFor="pending"
@@ -121,6 +122,7 @@ export default function EditInvoiceForm({
                   value="paid"
                   defaultChecked={invoice.status === "paid"}
                   className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                  aria-describedby="status-error"
                 />
                 <label
                   htmlFor="paid"
