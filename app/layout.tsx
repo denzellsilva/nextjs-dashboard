695f52b5024db5eecefba95df72365a1a66c79ef
Marl Denzell J. Silva <silvamarldenzell@gmail.com>
2026-03-02 03:07:59 +0800

    chacha
    Added a comment for testing purposes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
 import { Metadata } from "next";
 
+// new test commit 
+
 export const metadata: Metadata = {
   title: {
     template: "%s | Acme Dashboard",
